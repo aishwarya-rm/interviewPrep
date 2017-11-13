a468c9f699d40bf035e445d52b45797ce6f2410b
Aishwarya Mandyam <aishrm2@uw.edu>
2017-11-12 16:28:53 -0800

    added URLify.java

---
+++ URLify.java
@@ -0,0 +1,16 @@
+class URLify{
+  public static void main(String[] args){
+
+  }
+  public static String URLify(String s){
+    char[] sArr = s.toCharArray();
+    for(int i = 0; i < sArr.length; i++){
+      if(s.charAt(i) == " "){
+        //Move everything from the next index over three indeces
+        for(int j = i+1; )
+        //Insert the characters here
+      }
+    }
+    return sArr;
+  }
+}
