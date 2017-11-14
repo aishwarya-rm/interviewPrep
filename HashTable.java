c94c6a7658da9263a0a62b935d6cae1b7e4c8d91
Aishwarya Mandyam <aishwarya_m@live.com>
2017-11-14 12:30:14 -0800

    started implementation of hashtable

---
+++ HashTable.java
@@ -0,0 +1,17 @@
+class HashTable {
+  int[] keys = new int[10];
+  int[] values = new int[10];
+  
+  public void put(int key, int value){
+
+  }
+  public int[] resize(int[] arr){
+
+  }
+  public int get(int key){
+
+  }
+  public int hash(int key, int value){
+
+  }
+}
