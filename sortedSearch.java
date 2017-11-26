5641c8a3b8a14b986ffda8c341d1796c404f809f
Aishwarya Mandyam <aishwarya_m@live.com>
2017-11-25 18:08:36 -0800

    listy

---
+++ sortedSearch.java
@@ -0,0 +1,12 @@
+//figure out the length of this list in O(logn) time and then perform mostly binary search. That's annoying. 
+class sortedSearch{
+  public static void main(String[] args){
+
+  }
+  public static int find(int[] arr, int k){
+    if(k < elementAt(i)){
+      return -1;
+    }
+
+  }
+}
