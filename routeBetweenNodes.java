a808f2b9aa3fadc4f158d34b567fc73f7c410378
Aishwarya Mandyam <aishwarya_m@live.com>
2017-12-03 14:27:23 -0800

    routebetweennodes alg

--- routeBetweenNodes.java
+++ routeBetweenNodes.java
@@ -2,9 +2,18 @@ class routeBetweenNodes{
   public static void main(String[] args){
 
   }
-  public static boolean route(Graph g, Node start, Node end){
+  public static boolean route(Node g, Node start, Node end){
+    boolean seenStart = False;
+    boolean seenEnd = False;
     //need to find the start Node
-    
+    //Use BFS to find the start node
+    Queue<Node> q = new Queue<Node>();
+    q.push(g);
+    while(!q.isEmpty()){
+      Node curr = q.pop();
+      if(curr == start){
+      }
+    }
     //check to see from there if there is a path to the end node
   }
 }
