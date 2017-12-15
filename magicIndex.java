3380b991d5007a35b57cacfea875d56dc8135b9a
Aishwarya Mandyam <aishwarya_m@live.com>
2017-12-15 11:15:20 -0800

    style please

--- magicIndex.java
+++ magicIndex.java
@@ -1,8 +1,13 @@
+/* Given an array, find the index i such that A[i] == i. This is called the
+ * magic index */
+
 class magicIndex{
+  /* Testing */
   public static void main(String[] args){
     System.out.println(magicIndex2(new int[]{-1, 0, 1, 3}));
   }
-  //Naive approach
+
+  /* Naive approach: Strategy is just to do a linear search */
   public static int magicIndex(int[] arr){
     for(int i = 0; i < arr.length; i++){
       if(arr[i] > arr.length - 1){
@@ -14,16 +19,19 @@ class magicIndex{
     }
     return -1;
   }
-  //Binary search approach
+
+  /* More complex: Strategy is to do a binary search through the array */
   public static int magicIndex2(int[] arr){
     int low = 0;
     int high = arr.length - 1;
-    while(low < high){
+    while(low <= high){
       int mid = (high + low)/2;
+      System.out.println("Low: " + low);
+      System.out.println("High: " + high);
+      System.out.println("Mid: " + mid);
       if(arr[mid] == mid){
         return mid;
       }
-      System.out.println(mid);
       if(arr[mid] > mid){
         high = mid - 1;
       }
