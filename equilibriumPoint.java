7fe0ab61d474ea5526e5a8814a29346d56ac3601
Aishwarya Mandyam <aishwarya_m@live.com>
2017-12-21 17:13:50 -0800

    second version of eq point

--- equilibriumPoint.java
+++ equilibriumPoint.java
@@ -17,8 +17,24 @@ class equilibriumPoint{
       else{
         totalSum -= arr[i];
         leftSum += arr[i];
+
       }
     }
     return -1;
+}
+  public static int pivotPoint(int[] arr){
+    int totalSum = 0;
+    for(int i = 0; i < arr.length; i++){
+      totalSum += arr[i];
+    }
+    int leftSum = 0;
+    for(int i = 0; i < arr.length; i++){
+      if(totalSum - leftSum - arr[i] == leftSum){
+        return i;
+      }
+      leftSum += arr[i];
+    }
+    return -1;
   }
+
 }
