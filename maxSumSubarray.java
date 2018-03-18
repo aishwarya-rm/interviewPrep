c88a12f7fcd4ddb81f6d4dd938f92ae92bf6c9cb
Aishwarya Mandyam <aishrm2@cs.washington.edu>
2018-03-18 13:48:53 -0700

    brute force of max sum subarray

---
+++ maxSumSubarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maxSumSubarray {
+    public static void main(String[] args) {
+
+    }
+
+    public static int bruteForce(int[] nums) {
+        int overallMax = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum > overallMax) {
+                    overallMax = sum;
+                }
+            }
+        }
+        return overallMax;
+    }
+}
